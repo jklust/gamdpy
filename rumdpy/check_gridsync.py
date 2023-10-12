e95239cff9564cebb75d085f2c25531d220fad61
Ulf R. Pedersen <ulf@urp.dk>
2023-10-12 14:17:36 +0200

    Added scipt/function that can check if gridsync is avalibe

---
+++ rumdpy/check_gridsync.py
@@ -0,0 +1,46 @@
+""" Check CUDA availability, versions, and test if gridsync is supported. """
+
+
+def gridsync_example():
+    """ Example from https://numba.readthedocs.io/en/stable/cuda/cooperative_groups.html """
+    from numba import cuda, int32
+
+    sig = (int32[:, ::1],)
+
+    @cuda.jit(sig)
+    def sequential_rows(M):
+        col = cuda.grid(1)
+        g = cuda.cg.this_grid()
+
+        rows = M.shape[0]
+        cols = M.shape[1]
+
+        for row in range(1, rows):
+            opposite = cols - col - 1
+            M[row, col] = M[row - 1, opposite] + 1
+            g.sync()
+
+
+def check_gridsync(verbose=True):
+    """ Check CUDA availability, versions, and test if gridsync is supported. Returns True if gridsync is supported. """
+    import numba
+    from numba import cuda
+
+    if verbose:
+        print(f'{numba.__version__ = }')
+        print(f'{numba.cuda.is_available() = }')
+        print(f'{numba.cuda.is_supported_version() = }')
+        print(f'{cuda.runtime.get_version() = }')
+
+    # See if gridsync is working
+    try:
+        gridsync_example()
+    except numba.cuda.cudadrv.driver.LinkerError as e:
+        if verbose:
+            print('Warning: gridsync is not supported')
+        return False
+
+    return True
+
+if __name__ == '__main__':
+    check_gridsync()
