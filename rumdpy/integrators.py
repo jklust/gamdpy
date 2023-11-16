b7433e1deb9b8556ead1aecc75e226a5e3608c38
Ulf R. Pedersen <ulf@urp.dk>
2023-11-16 15:36:28 +0100

    started thinking about implimenting the Langevin NVT thermostat

--- rumdpy/integrators.py
+++ rumdpy/integrators.py
@@ -117,7 +117,7 @@ def make_step_nvt(configuration, compute_plan, verbose=True,):
         print(f'Generating NVT integrator for {num_part} particles in {D} dimensions:')
         print(f'\tpb: {pb}, tp:{tp}, num_blocks:{num_blocks}')
         print(f'\tNumber (virtual) particles: {num_blocks*pb}')
-        print(f'\tNumber of threads {num_blocks*pb*tp}')      
+        print(f'\tNumber of threads {num_blocks*pb*tp}')
 
     # Unpack indicies for vectors and scalars    
     for key in configuration.vid:
@@ -203,7 +203,75 @@ def make_step_nvt(configuration, compute_plan, verbose=True,):
             step_nvt[num_blocks, (pb, 1)](grid, vectors, scalars, r_im, sim_box, integrator_params)
             update_thermostat_state[1, (1, 1)](integrator_params)
             return
-        return step 
+        return step
 
+
+def make_run_langevin_nvt(configuration, compute_plan, verbose=True, ):
+    raise NotImplementedError('make_run_langevin_nvt is not implemented yet')
+
+    pb = compute_plan['pb']
+    tp = compute_plan['tp']
+    gridsync = compute_plan['gridsync']
+    D = configuration.D
+    num_part = configuration.N
+    num_blocks = (num_part - 1) // pb + 1
+
+    if verbose:
+        print(f'Generating NVT langevin integrator for {num_part} particles in {D} dimensions:')
+        print(f'\tpb: {pb}, tp:{tp}, num_blocks:{num_blocks}')
+        print(f'\tNumber (virtual) particles: {num_blocks * pb}')
+        print(f'\tNumber of threads {num_blocks * pb * tp}')
+
+    # Unpack indices for vectors and scalars
+    for key in configuration.vid:
+        exec(f'{key}_id = {configuration.vid[key]}', globals())
+    for key in configuration.sid:
+        exec(f'{key}_id = {configuration.sid[key]}', globals())
+
+    # @cuda.jit('void(float32[:,:,:], float32[:,:], int32[:,:], float32[:], float32)', device=gridsync)
+    # @cuda.jit(device=gridsync)
+    def step_nvt(grid, vectors, scalars, r_im, sim_box, integrator_params):
+        """ Make one timestep using Leap-frog
+            Kernel configuration: [num_blocks, (pb, tp)]
+        """
+
+        dt, = integrator_params
+
+        my_block = cuda.blockIdx.x
+        local_id = cuda.threadIdx.x
+        global_id = my_block * pb + local_id
+        my_t = cuda.threadIdx.y
+
+        if global_id < num_part and my_t == 0:
+            my_r = vectors[r_id][global_id]
+            my_v = vectors[v_id][global_id]
+            my_f = vectors[f_id][global_id]
+            my_m = scalars[global_id][m_id]
+            my_k = numba.float32(0.0)  # Kinetic energy
+            my_fsq = numba.float32(0.0)  # force squared energy
+
+            for k in range(D):
+                my_fsq += my_f[k] * my_f[k]
+                my_v[k] += numba.float32(0.5) * my_f[k] / my_m * dt
+                my_k += numba.float32(0.5) * my_m * my_v[k] * my_v[k]
+                my_v[k] += numba.float32(0.5) * my_f[k] / my_m * dt
+                my_r[k] += my_v[k] * dt
+                if my_r[k] * numba.float32(2.0) > sim_box[k]:
+                    my_r[k] -= sim_box[k]
+                    r_im[global_id, k] += 1
+                if my_r[k] * numba.float32(2.0) < -sim_box[k]:
+                    my_r[k] += sim_box[k]
+                    r_im[global_id, k] -= 1
+                # if my_r[k]*numba.float32(2.0) > sim_box[k] or  my_r[k]*numba.float32(2.0) < -sim_box[k]:
+                #    print(global_id, k, my_r[0], my_r[1], my_r[2])
+                # vectors[r_id][global_id,k] = my_r[k]
+            scalars[global_id][k_id] = my_k
+            scalars[global_id][fsq_id] = my_fsq
+        return
+
+    if gridsync:
+        return cuda.jit(device=gridsync)(step_nvt)  # return device function
+    else:
+        return cuda.jit(device=gridsync)(step_nvt)[num_blocks, (pb, 1)]  # return kernel, incl. launch parameters
 
 
