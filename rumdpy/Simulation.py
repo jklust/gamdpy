e3cda69634e02c3b2c221f8e67b50f0918ceb276
tbs.cph <tbs.cph@gmail.com>
2024-01-24 18:44:30 +0100

    Simulation.py updated to store vectors and scalars (needed for e.g. 3D visualizarion)

--- rumdpy/Simulation.py
+++ rumdpy/Simulation.py
@@ -50,7 +50,11 @@ class simulation():
             # allocation delayed until beginning of run to let user reconsider
         else:
             print("WARNING: Results will not be stored. To change this use storage='hdf5' or 'memory'")
-
+            
+        self.vectors_list = []
+        self.scalars_list = []
+        self.simbox_data_list = []
+        self.scalars_t = []
             
     def run(self, num_blocks=-1):
         if num_blocks==-1:
@@ -60,15 +64,18 @@ class simulation():
         if self.storage=='memory':
             self.conf_blocks = np.zeros((self.num_blocks, self.conf_per_block, self.num_vectors, self.conf.N, self.conf.D), dtype=np.float32)
         
-        tt = []
-        conf_list = []
-        scalars_t = []
+        self.vectors_list = []
+        self.scalars_list = []
+        self.simbox_data_list = []
+        self.scalars_t = []
+        
         start = cuda.event()
         end = cuda.event()
         zero = np.float32(0.0)
         start.record()
         
         for block in range(num_blocks):
+            self.current_block = block
  
             self.d_output_array = cuda.to_device(self.zero_output_array) # Set output array to zero. Could probably be done faster
             self.integrator[0](self.conf.d_vectors, self.conf.d_scalars, self.conf.d_ptype, self.conf.d_r_im, self.conf.simbox.d_data,  
@@ -76,7 +83,7 @@ class simulation():
                                  self.d_output_array, self.d_conf_array, 
                                  np.float32(block*self.steps_per_block*self.dt), self.steps_per_block)
         
-            scalars_t.append(self.d_output_array.copy_to_host())        
+            self.scalars_t.append(self.d_output_array.copy_to_host())        
             
             if self.storage=='hdf5':
                 with h5py.File(self.filename+".h5", "a") as f:
@@ -91,6 +98,12 @@ class simulation():
                 rdf_calculator(c1.d_vectors, c1.simbox.d_data, c1.d_ptype, pairs['interaction_params'], d_gr_bins)
                 gr_bins.append(d_gr_bins.copy_to_host())
                 d_gr_bins = cuda.to_device(gr_bins_zeros)
+                
+            # By default for now:
+            self.conf.copy_to_host()
+            self.vectors_list.append(self.conf.vectors.copy())
+            self.scalars_list.append(self.conf.scalars.copy())
+            self.simbox_data_list.append(self.conf.simbox.data.copy())
 
             yield block
     
@@ -101,6 +114,8 @@ class simulation():
         self.timing_numba = cuda.event_elapsed_time(start, end)
         #self.nbflag = LJ.nblist.d_nbflag.copy_to_host()    
         self.nbflag = self.interactions['interaction_params'][4].copy_to_host() # HACK!
+        
+        self.scalars_list = np.array(self.scalars_list)
         
     def print_summary(self):
         tps = self.last_num_blocks*self.steps_per_block/self.timing_numba*1000
