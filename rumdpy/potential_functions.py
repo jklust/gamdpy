ff94b53daa440a2bed52d2b3e028547a2e0d1748
tbs.cph <tbs.cph@gmail.com>
2024-04-16 14:28:19 +0200

    LJ_12_6_sigma_epsilon: Corrected sign error on attractice part

--- rumdpy/potential_functions.py
+++ rumdpy/potential_functions.py
@@ -17,14 +17,19 @@ def LJ_12_6(dist, params):            # LJ: U(r)  =        A12*r**-12 +     A6*r
 
 
 def LJ_12_6_sigma_epsilon(dist, params):  # LJ:  U(r)  =     4*epsilon(   (r/sigma)**-12 +   (r/sigma)**-6 )
+    """
+    The 12-6 Lennard-Jones potential:
+    U(r)  =     4*epsilon(   (r/sigma)**-12 -   (r/sigma)**-6 )
+    Order of parameters: sigma, epsilon, cutoff
+    """
     sigma = params[0]                     #      Um(r) =   -24*epsilon( 2*(r/sigma)**-13 +   (r/sigma)**-7 )/sigma
     epsilon = params[1]                   #      Umm(r) =   24*epsilon(26*(r/sigma)**-14 + 7*(r/sigma)**-8 )/sigma**2
     OneOdist = numba.float32(1.0)/dist    # s = -Um/r =     24*epsilon( 2*(r/sigma)**-14 +   (r/sigma)**-8 )/sigma**2,  Fx = s*dx
     sigmaOdist = sigma*OneOdist
 
-    u =   numba.float32( 4.0)*(                     sigmaOdist**12 +                    sigmaOdist**6 )
-    s =   numba.float32(24.0)*( numba.float32( 2.0)*sigmaOdist**12 +                    sigmaOdist**6 )*OneOdist**2
-    umm = numba.float32(24.0)*( numba.float32(26.0)*sigmaOdist**12 + numba.float32(7.0)*sigmaOdist**6 )*OneOdist**2
+    u =   numba.float32( 4.0)*(                     sigmaOdist**12 -                    sigmaOdist**6 )
+    s =   numba.float32(24.0)*( numba.float32( 2.0)*sigmaOdist**12 -                    sigmaOdist**6 )*OneOdist**2
+    umm = numba.float32(24.0)*( numba.float32(26.0)*sigmaOdist**12 - numba.float32(7.0)*sigmaOdist**6 )*OneOdist**2
     return u, s, umm # U(r), s == -U'(r)/r, U''(r)
 
 
