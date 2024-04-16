aab858baeb74e526158d4f188261ee7ce5e20b26
tbs.cph <tbs.cph@gmail.com>
2024-04-16 15:16:29 +0200

    examples/minimal.py: Gives potential parameters as required by (new) PairPotential2. Minor tweaks.

--- examples/minimal.py
+++ examples/minimal.py
@@ -6,22 +6,23 @@ Simulation of a Lennard-Jones crystal in the NVT ensemble.
 
 import rumdpy as rp
 
-# Setup fcc configuration
+# Setup configuration: FCC Lattice
 configuration = rp.make_configuration_fcc(nx=8, ny=8, nz=8, rho=0.973, T=0.8 * 2)
 
-# Setup pair potential.
-pairpot_func = rp.apply_shifted_force_cutoff(rp.LJ_12_6)
-params = [[[4.0, -4.0, 2.5], ], ]
-pair_potential = rp.PairPotential2(pairpot_func, params=params, max_num_nbs=1000)
+# Setup pair potential: Single component 12-6 Lennard-Jones
+pairfunc = rp.apply_shifted_force_cutoff(rp.LJ_12_6_sigma_epsilon)
+sig, eps, cut = 1.0, 1.0, 2.5
+pairpot = rp.PairPotential2(pairfunc, params=[sig, eps, cut], max_num_nbs=1000)
 
-# Setup integrator
+# Setup integrator: NVT
 integrator = rp.integrators.NVT(temperature=0.70, tau=0.2, dt=0.005)
 
 # Setup Simulation. Total number of timesteps: num_blocks * steps_per_block
 num_blocks = 16
 steps_per_block = 1024*2
-sim = rp.Simulation(configuration, pair_potential, integrator, 
-                    num_blocks, steps_per_block, storage='LJ_T0.70.h5') 
+sim = rp.Simulation(configuration, pairpot, integrator,
+                    num_blocks, steps_per_block,
+                    storage='LJ_T0.70.h5')
 
 # Run simulation one block at a time
 for block in sim.blocks():
