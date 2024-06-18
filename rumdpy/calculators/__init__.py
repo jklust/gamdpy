81c287b466e53d381693bf47e062ee7cb131f303
urpedersen <ulf@urp.dk>
2024-06-18 17:01:57 +0200

    Added Calculator for structure factor and test

--- rumdpy/calculators/__init__.py
+++ rumdpy/calculators/__init__.py
@@ -1,5 +1,6 @@
 from .rdf import *
 from .CalculatorRadialDistribution import *
+from .CalculatorStructureFactor import *
 from .scalar_calculator import *
 from .conf_saver import *
 from .runtime_action_executor import *
