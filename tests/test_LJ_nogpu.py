0789842c409eb4dd6885eb8e2f7b6305ef1be456
tbs.cph <tbs.cph@gmail.com>
2024-06-25 00:37:13 +0200

    add test_LJ_nogpu.py testing setup of simulation, but not running, so test can run without gpu

---
+++ tests/test_LJ_nogpu.py
@@ -0,0 +1,54 @@
+import unittest
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+       pass 
+
+    def test_(self):
+        import rumdpy as rp
+        import numpy as np
+        
+        # Generate configurations with a FCC lattice
+        configuration1 = rp.make_configuration_fcc(nx= 8, ny= 8, nz=8,  rho=0.8442, T=1.44)  
+        configuration2 = rp.make_configuration_fcc(nx= 5, ny= 5, nz=13, rho=1.2000, T=0.44)  
+        configuration3 = rp.make_configuration_fcc(nx=16, ny=16, nz=32, rho=0.8442, T=2.44)  
+
+        # Make pair potentials
+        pairfunc = rp.apply_shifted_force_cutoff(rp.LJ_12_6_sigma_epsilon)
+        sig, eps, cut = 1.0, 1.0, 2.5
+        pairpot1 = rp.PairPotential2(pairfunc, params=[sig, eps, cut], max_num_nbs=1000)
+
+        pairfunc = rp.apply_shifted_potential_cutoff(rp.LJ_12_6_sigma_epsilon)
+        sig = [[1.00, 0.80],
+               [0.80, 0.88]]
+        eps = [[1.00, 1.50],
+               [1.50, 0.50]]
+        cut = np.array(sig)*2.5
+        pairpot2 = rp.PairPotential2(pairfunc, params=[sig, eps, cut], max_num_nbs=1000)
+
+        # Make integrators
+        dt = 0.005 # timestep 
+        temperature = 0.7 # Not used for NVE
+        pressure = 1.2 # Not used for NV*
+
+        integrator1 = rp.integrators.NVE(dt=dt)
+        integrator2 = rp.integrators.NVE_Toxvaerd(dt=dt)
+        integrator3 = rp.integrators.NVT(temperature=temperature, tau=0.2, dt=dt)
+        integrator4 = rp.integrators.NVT_Langevin(temperature=temperature, alpha=0.2, dt=dt, seed=2023)
+        integrator5 = rp.integrators.NPT_Langevin(temperature=temperature, pressure=pressure, 
+                                                alpha=0.1, alpha_baro=0.0001, mass_baro=0.0001, 
+                                                volume_velocity=0.0, barostatModeISO = True , boxFlucCoord = 2,
+                                                dt=dt, seed=2023)
+
+        for configuration in [configuration1, configuration2, configuration3]:
+            for pairpot in [pairpot1, pairpot2]:
+                ev = rp.Evaluater(configuration, pairpot)
+                for integrator in [integrator1, integrator2, integrator3, integrator4, integrator5]:
+                    sim = rp.Simulation(configuration, pairpot, integrator,
+                                        num_blocks=64, steps_per_block=1024, storage='memory')
+
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
