3c782ff1e643421f092e5bd111140542f12f56bb
tbs.cph <tbs.cph@gmail.com>
2024-07-01 11:21:03 +0200

    minimal.py updated to steps_between_momentum_reset being non-default parameter of class Simulation

--- examples/minimal.py
+++ examples/minimal.py
@@ -20,6 +20,7 @@ integrator = rp.integrators.NVT(temperature=0.7, tau=0.2, dt=0.005)
 
 # Setup Simulation. 
 sim = rp.Simulation(configuration, pair_pot, integrator,
+                    steps_between_momentum_reset=100,
                     num_steps=32*1024, storage='LJ_T0.70.h5')
 
 # Run simulation
