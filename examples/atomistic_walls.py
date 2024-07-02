3839ba731a48831adcb2fb9dd1b4a3d4fd798e9c
Jesper Schmidt Hansen <jschmidt@nb-dk1081102.ruc.dk>
2024-07-02 11:57:01 +0200

    Final checks and Comments to example file

--- examples/atomistic_walls.py
+++ examples/atomistic_walls.py
@@ -1,75 +1,96 @@
-""" Example of a nanoslit pore simulation using rumdpy.
+""" Example of a nanoslit pore simulation using tethered LJ particles 
 
+    The particles are tethered with a Hooke spring.   
+
+    Wall density is set to 1.0 and fluid density to 0.8 - this is achieved by inclusion of a dummy particle
+    Currently the entire system is thermostated. 
+
+    Initial and final configurations are saved in xyz format for easy inspection in vmd
 """
+
 import numpy as np
 import random 
 import rumdpy as rp
 
-springconstant = 300.0
+def savexyz(configuration, filestr):
+    
+    configuration.get_vector('r')
+    npart = configuration.N
 
-# Setup configuration: FCC Lattice
-nx, ny, nz, rhoWall, rhoFluid = 12, 12, 20, 1.0, 0.8;
-configuration = rp.make_configuration_fcc(nx, ny, nz, rhoWall)
+    fp =  open(filestr,"w")
+    fp.write("%d\n" % (npart))
+    fp.write("Test for tethered particles\n")
+    for n in range(npart):
+        fp.write("%d %f %f %f\n" % 
+                 (configuration.ptype[n], configuration['r'][n][0], configuration['r'][n][1], configuration['r'][n][2])) 
+    fp.close()
+
+
+# Setup a default fcc configuration
+nxUnits, nyUnits, nzUnits, rhoWall, rhoFluid = 6, 6, 10, 1.0, 0.8;
+configuration = rp.make_configuration_fcc(nxUnits, nyUnits, nzUnits, rhoWall)
 
 # Fluid particles have type '0', wall particles '1', dummy particles '2'
-nwall, npart = nx*ny*4, nx*ny*nz
-configuration.ptype[:nwall-1] = 1
-configuration.ptype[npart-nwall-1:] = 1
+nwall, npart = 0, configuration.N
+hlz = 0.5*configuration.simbox.lengths[2]
+for n in range(npart):
+    if configuration['r'][n][2] + hlz < 5.0:
+        configuration.ptype[n] = 1
+        nwall = nwall + 1
+
 nfluid = np.sum( configuration.ptype==0 )
 nfluidWanted = int(nfluid*rhoFluid/rhoWall)
 
 while nfluid > nfluidWanted:
-    idx = random.randint(nwall, npart-nwall)
+    idx = random.randint(0,npart-1)
     if configuration.ptype[idx] == 0:
         configuration.ptype[idx] = 2
         nfluid = nfluid - 1
 
-# Tether the wall particles
-particle_indices = []
+savexyz(configuration, "initial.xyz")
+
+# Tether specifications. 
+# Tether-parameters: [x0, y0, z0, kspring] ; Index array: [row index in param, particle/atom index]  
+springconstant = 1000.0
+indices_array = []
 tether_parameters = []
+counter = 0
 for n in range(npart):
     if configuration.ptype[n]==1:
-        particle_indices.append(n)
+        indices_array.append([counter, n])
+        counter = counter + 1
         pos =  configuration['r'][n]
-        tether_parameters.append( [pos[0], pos[1], pos[2], 300.0] )
+        tether_parameters.append( [pos[0], pos[1], pos[2], springconstant] )
 
-#particle_indices = np.where(configuration.ptype==1)[0] #<- convert to list comprehension
-#tether_parameters = np.concatenate( (configuration['r'][particle_indices], np.ones((nwall,1))*300 ), axis=1)
-#springs = [np.ones( (nwall,1) )*springconstant] 
-
-tether = rp.Tether(tether_parameters, particle_indices)
+tether = rp.Tether(tether_parameters, indices_array, verbose=False)
 
 # Set the pair interactions
 pairfunc = rp.apply_shifted_potential_cutoff(rp.LJ_12_6_sigma_epsilon)
-sig = [[1.0, 1.0, 0.0], 
-       [1.0, 1.0, 0.0], 
-       [0.0, 0.0, 0.0]] 
-eps = [[1.0, 1.0, 0.0], 
-       [1.0, 1.0, 0.0], 
-       [0.0, 0.0, 0.0]]
+sig = [[1.0, 1.0, 0.0], [1.0, 1.0, 0.0], [0.0, 0.0, 0.0]] 
+eps = [[1.0, 1.0, 0.0], [1.0, 1.0, 0.0], [0.0, 0.0, 0.0]]
 cut = np.array(sig)*2.5 
 pairpot = rp.PairPotential2(pairfunc, params=[sig, eps, cut], max_num_nbs=1000)
 
-    # Temperature
-configuration.randomize_velocities(T=0.8 * 2)
+# Temperature
+configuration.randomize_velocities(T=0.7)
 
 # Setup integrator: NVT
 integrator = rp.integrators.NVT(temperature=0.70, tau=0.2, dt=0.005)
+
+# Some compute plan settings for old cards 
 compute_plan = rp.get_default_compute_plan(configuration)
 compute_plan['gridsync']=False
-compute_plan['tp']=1 
+compute_plan['tp']=2 
 
 # Setup Simulation. Total number of timesteps: num_blocks * steps_per_block
-sim = rp.Simulation(configuration, [pairpot,tether], integrator,
-                    num_timeblocks=16, steps_per_timeblock=1024 * 2,
+sim = rp.Simulation(configuration, [pairpot, tether], integrator,
+                    num_timeblocks=12, steps_per_timeblock=6*1024,
                     storage='LJ_T0.70.h5', compute_plan=compute_plan)
-
-print(sim.compute_plan)
 
 # Run simulation one block at a time
 for block in sim.timeblocks():
     print(sim.status(per_particle=True))
+
 print(sim.summary())
 
-# To get a plot of the MSD do something like this:
-# python -m rumdpy.tools.calc_dynamics -f 4 -o msd.pdf LJ_T*.h5
+savexyz(configuration, "final.xyz")
--- rumdpy/interactions/tether.py
+++ rumdpy/interactions/tether.py
@@ -9,18 +9,20 @@ import rumdpy as rp
 
 class Tether():
 
-    def __init__(self, tether_params, particle_indices):
-     
-        self.tether_params = np.array(tether_params, dtype=float)
-        self.particle_indices = np.array(particle_indices, dtype=int) 
+    def __init__(self, tether_params, indices_array, verbose=False):
 
-        if self.tether_params.shape[0] != self.particle_indices.shape[0]:
+        self.tether_params = np.array(tether_params, dtype=np.float32)
+        self.indices_array = np.array(indices_array, dtype=np.int32) 
+
+        if self.tether_params.shape[0] != self.indices_array.shape[0]:
             raise ValueError("Length of indicies must be the same as length of tether points") 
 
+        if verbose:
+            print(f"{self.tether_params} \n {self.indices_array}")
 
     def get_params(self, configuration, compute_plan, verbose=False):
 
-        self.d_pindices = cuda.to_device(self.particle_indices)
+        self.d_pindices = cuda.to_device(self.indices_array)
         self.d_tether_params = cuda.to_device(self.tether_params);
         
         return (self.d_pindices, self.d_tether_params)
@@ -28,32 +30,32 @@ class Tether():
 
     def get_kernel(self, configuration, compute_plan, compute_stresses=False, verbose=False):
         # Unpack parameters from configuration and compute_plan
-        #D, N = configuration.D, configuration.N
-        #pb, tp, gridsync, UtilizeNIII = [compute_plan[key] for key in ['pb', 'tp', 'gridsync', 'UtilizeNIII']] 
-        #num_blocks = (N - 1) // pb + 1
+        D, N = configuration.D, configuration.N
+        pb, tp, gridsync, UtilizeNIII = [compute_plan[key] for key in ['pb', 'tp', 'gridsync', 'UtilizeNIII']] 
+        num_blocks = (N - 1) // pb + 1
     
         # Get indices values (instead of dictonary entries) 
-        #r_id, f_id = [configuration.vectors.indices[key] for key in ['r', 'f']]
-        #u_id, w_id, lap_id, m_id = [configuration.sid[key] for key in ['u', 'w', 'lap', 'm']]
+        r_id, f_id = [configuration.vectors.indices[key] for key in ['r', 'f']]
+        u_id, w_id, lap_id, m_id = [configuration.sid[key] for key in ['u', 'w', 'lap', 'm']]
 
-        #dist_sq_dr_function = numba.njit(configuration.simbox.dist_sq_dr_function)
-        #potential_function = numba.njit(rp.harmonic_bond_function)
-
-       # virial_factor = numba.float32( 0.5/configuration.D)
-
-        # values -> params
-        def tether_calculator(vectors, scalars, ptype, sim_box, indices, values):
+        dist_sq_dr_function = numba.njit(configuration.simbox.dist_sq_dr_function)
         
-            #dr = cuda.local.array(shape=D,dtype=numba.float32)
-            #dist_sq = dist_sq_dr_function(values[:D], vectors[r_id][indices[0]], sim_box, dr)
-            #u, s, umm = potential_function(math.sqrt(dist_sq), [0.0, values[indices[0], 3]])
+        def tether_calculator(vectors, scalars, ptype, sim_box, indices, values):
+       
+            dr = cuda.local.array(shape=D,dtype=numba.float32)
+            dist_sq = dist_sq_dr_function(values[indices[0]][:D], vectors[r_id][indices[1]], sim_box, dr)
             
-            #for k in range(D):
-               # cuda.atomic.add(vectors, (f_id, indices[0], k), -dr[k]*s)      # Force
+            spring = values[indices[0]][3]
+            u = numba.float32(0.5)*spring*dist_sq
+            s = -spring
+            umm = spring
+
+            for k in range(D):
+                cuda.atomic.add(vectors, (f_id, indices[1], k), -dr[k]*s)      # Force
                # cuda.atomic.add(scalars, (indices[0], w_id), dr[k]*dr[k]*s*virial_factor)    # Virial
                # cuda.atomic.add(scalars, (indices[1], w_id), dr[k]*dr[k]*s*virial_factor)                      
         
-            #cuda.atomic.add(scalars, (indices[0], u_id), u*numba.float32(0.5)) # Potential energy 
+            cuda.atomic.add(scalars, (indices[1], u_id), u*numba.float32(0.5)) # Potential energy 
             #cuda.atomic.add(scalars, (indices[1], u_id), u*numba.float32(0.5))
             #lap = numba.float32(1-D)*s + umm                                   # Laplacian  
             #cuda.atomic.add(scalars, (indices[0], lap_id), lap)               
