e27de5873256d4e2d5e31341515b94ccbdb6be52
lorenzo-costigliola <lorenzo.costigliola@gmail.com>
2024-07-09 20:33:23 +0200

    Added opportunity to read an entire rumd3 simulation in rumdpy format and example on how to calculate the rdf using rumdpy
    in rumdpy/tools/load_output.py implemented load_rumd3 function which allows for reading the entire output contained in the rumd3 TrajectoryFiles (not restarts). This might be useful for testing of tools and comparing with old rumd3 versions
    
    the example calc_rdf_from_rumd3.py uses load_output to read a rumd3 TrajectoryFiles and calculate the rdf. The rdf is calculated using the GPU

---
+++ examples/calc_rdf_from_rumd3.py
@@ -0,0 +1,37 @@
+""" Minimal example for calculing rdf from existing data """
+
+import rumdpy as rp
+import numpy as np
+import os
+
+if "LJ_T0.70.h5" not in os.listdir(os.getcwd()):
+    print("This example needs a the file LJ_T0.70.h5 to be present")
+    print("LJ_T0.70.h5 can be generated using minimal.py example")
+    exit()
+
+# Create configuration object
+configuration = rp.Configuration()
+# Load existing data
+output = rp.tools.load_output("TrajectoryFiles")
+# Read number of particles N and dimensions from data
+nblocks, nconfs, _ , N, D = output['block'].shape
+# Set up the configuration object
+configuration['r'] = output['block'][0][0][0]
+configuration.simbox = rp.Simbox(D, output['attrs']['simbox_initial'])
+configuration.copy_to_device()
+# Call the rdf calculator
+calc_rdf = rp.CalculatorRadialDistribution(configuration, num_bins=1000)
+
+# NOTE: the structure of the block is (outer_block, inner_steps, pos&img, npart, dimensions)
+#       the zero is to select the position array and discard images
+positions = output['block'][:,:,0,:,:]
+positions = positions.reshape(nblocks*nconfs,N,D)
+# Loop over saved configurations
+for pos in positions[nconfs-1::nconfs]:
+    configuration.set_vector('r', pos)
+    configuration.copy_to_device()
+    calc_rdf.update()
+
+# Save rdf
+calc_rdf.save_average("rdf_rumd3.dat")
+
--- rumdpy/tools/load_output.py
+++ rumdpy/tools/load_output.py
@@ -30,9 +30,103 @@ def load_h5(name:str) -> dict:
     return output
 
 # Load from TrajectoryFiles (std rumd3 output)
+# It assumes trajectories are spaced in log2
 def load_rumd3(name:str) -> dict:
-    import gzip
-    return
+    import os, gzip, glob
+    import pandas as pd
+    # Check what's there
+    energy, traj  = True, True
+    # Rumd3 output is always in D=3
+    dim = 3
+    # Defining output dictionary
+    output = dict()
+    if "LastComplete_energies.txt" not in os.listdir(name):
+        print("LastComplete_energies.txt not present")
+        energy = False
+    if "LastComplete_trajectory.txt" not in os.listdir(name):
+        print("LastComplete_trajectory.txt not present")
+        traj = False
+    if not energy and not traj:
+        print("No LastComplete file found, exiting")
+        exit()
+    if not energy: nblocks, blocksize = np.loadtxt(f"{name}/LastComplete_energies.txt", dtype=np.int32)
+    else         : nblocks, blocksize = np.loadtxt(f"{name}/LastComplete_trajectory.txt", dtype=np.int32)
+    # Create list of files to read
+    if traj:
+        traj_files = sorted(glob.glob(f"{name}/trajectory*"))
+        # Read metadata from first file in the list
+        if traj_files[-1]==f"{name}/trajectory{nblocks+1:04d}.xyz.gz": traj_files = traj_files[:-1]
+        with gzip.open(f"{traj_files[0]}", "r") as f:
+            npart = int(f.readline())
+            cmt_line = f.readline().decode().split()
+            meta_data = dict()
+            for item in cmt_line:
+                key, val = item.split("=")
+                meta_data[key] = val
+            num_types = int(meta_data['numTypes'])
+            masses = [float(x) for x in  meta_data['mass'].split(',')]
+            assert len(masses) == num_types
+            if meta_data['ioformat'] == '1':
+                lengths = np.array([float(x) for x in meta_data['boxLengths'].split(',')], dtype=np.float32)
+            else:
+                assert meta_data['ioformat'] == '2'
+                sim_box_data = meta_data['sim_box'].split(',')
+                sim_box_type = sim_box_data[0]
+                sim_box_params = [float(x) for x in sim_box_data[1:]]
+                assert sim_box_type == 'RectangularSimulationBox'
+                lengths = np.array(sim_box_params, dtype=np.float32)
+                integrator_data = meta_data['integrator'].split(',')
+                timestep = integrator_data[1]
+        #print(npart, num_types, masses, sim_box_params, timestep, np.log2(blocksize))
+        # Loop over the files and read them assuming each line is type, x, y, z, imx, imy, imz
+        ntrajinblock = int(1+np.log2(blocksize))
+        toskip1 = np.array([  (npart+2)*x for x in range(1+ntrajinblock)])
+        toskip2 = np.array([1+(npart+2)*x for x in range(1+ntrajinblock)])
+        toskip  = sorted(list(np.concatenate((toskip1, toskip2))))
+        #print(toskip)
+        positions = list()
+        images    = list()
+        for trajectory in traj_files:
+            tmp_data   = pd.read_csv(trajectory, skiprows = toskip, usecols=[0,1,2,3,4,5,6], names=["type", "x", "y", "z", "imx", "imy", "imz"], delimiter=" ")
+            type_array = tmp_data['type'].to_numpy()
+            pos_array  = np.c_[tmp_data['x'].to_numpy(), tmp_data['y'].to_numpy(), tmp_data['z'].to_numpy()]
+            img_array  = np.c_[tmp_data['imx'].to_numpy(), tmp_data['imy'].to_numpy(), tmp_data['imz'].to_numpy()]
+            positions.append(pos_array.reshape((1+ntrajinblock,npart,dim)))
+            images.append(pos_array.reshape((1+ntrajinblock,npart,dim)))
+        # Saving data in output dictionary
+        output['block'] = np.zeros((len(traj_files), 1+ntrajinblock, 2, npart, dim))
+        output['block'][:,:,0,:,:] = np.array(positions) 
+        output['block'][:,:,0,:,:] = np.array(images) 
+        output['ptype'] = type_array[:npart]
+        output['attrs'] = {'dt': timestep, 'simbox_initial': lengths}
+    else:
+        output['block'] = None
+        output['ptype'] = None
+        output['attrs'] = None
+    if energy:
+        energy_files = sorted(glob.glob(f"{name}/energies*"))
+        # Read metadata from first file in the list
+        if energy_files[-1]==f"{name}/energies{nblocks+1:04d}.dat.gz": energy_files = energy_files[:-1]
+        with gzip.open(f"{energy_files[0]}", "r") as f:
+            # ioformat=2 N=4096 Dt=147.266830 columns=ke,pe,p,T,Etot,W
+            cmt_line = f.readline().decode().split()[1:]
+            meta_data = dict()
+            for item in cmt_line:
+                key, val = item.split("=")
+                meta_data[key] = val
+            npart = meta_data['N']
+            save_interval = meta_data['Dt']
+            col_names = meta_data['columns'].split(",")
+            all_energies = list()
+            for energies in energy_files:
+                tmp_data   = pd.read_csv(energies, names=col_names, delimiter=" ")
+                all_energies.append(tmp_data)
+            if output['attrs']!=None : output['steps_between_output'] = float(save_interval)/float(output['attrs']['dt'])
+            else                     : output['steps_between_output'] = None
+    else:
+        output['scalars'] = None
+        output['steps_between_output'] = None
+    return output
 
 if __name__ == '__main__':
     load_output(sys.argv[1])
