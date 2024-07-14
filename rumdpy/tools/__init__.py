6e49ce68ae873161489fe5f9c2f4c0e110b223a2
urpedersen <ulf@urp.dk>
2024-07-14 11:14:18 +0200

    Deleted get_scalar_sums.py and the example using it.

--- rumdpy/tools/__init__.py
+++ rumdpy/tools/__init__.py
@@ -2,4 +2,3 @@ from .calc_dynamics import *
 from .make_lattice import make_lattice
 from .save_configuration import *
 from .load_output import load_output
-from .get_scalar_sums import get_scalar_sums
