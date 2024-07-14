93afafe7bdf7ebe4c2d74caa7d3a356bbe8b863e
urpedersen <ulf@urp.dk>
2024-07-14 08:27:00 +0200

    More explanations in yukawa.py

--- examples/yukawa.py
+++ examples/yukawa.py
@@ -1,13 +1,26 @@
 """ Example of a user defined potential, example of a Yukawa potential.
 
+This script demonstrates how to define a user-defined potential function
+and use it in a rumdpy simulation. The example uses the Yukawa potential
+as an example, but the same approach can be used for other pair potentials.
+
+Comments
+--------
+
+The pair potential function is JIT compiled using numba.cuda,
+and it is often the time-consuming part of the simulation.
+Thus, you may want to optimize this function. For example,
+it is the experience that numba.float32 is faster than numba.float64.
+
+For mathematical functions supported by numba.cuda use the math module,
+as described in the numba documentation:
+
+    https://numba.readthedocs.io/en/stable/cuda/cudapysupported.html#math
+
 This example uses a syntax similar to the backend of rumdpy, making it easy to
 include the code in the package, and making it available to the community.
 
-Note:
-
-See
-    https://numba.readthedocs.io/en/stable/cuda/cudapysupported.html#math
-for supported math functions in numba.cuda.
+It is recommended ensuring that the analytical derivatives are correct.
 
 """
 
@@ -25,9 +38,8 @@ def yukawa(dist, params):
 
     parameters: κ, A    (κ is the greek letter kappa)
 
-    The Yukawa potential is a simple screened Coulomb potential. The potential
-    is used to model interactions between charged particles in a plasma. The
-    potential is given by:
+    The Yukawa potential is a simple screened Coulomb potential.
+    The potential is given by:
 
         u(r) = A·exp(-κ·r)/r
 
@@ -52,7 +64,7 @@ def yukawa(dist, params):
     kappa = numba.float32(params[0])  # κ
     prefactor = numba.float32(params[1])  # A
 
-    # Floats
+    # Floats. Note: numba.float32's may make code faster
     one = numba.float32(1.0)
     two = numba.float32(2.0)
 
@@ -68,12 +80,12 @@ def yukawa(dist, params):
     u = exp_kappa_dist * inv_dist
 
     # A·exp(-κ·r)·(κ·r + 1)/r³
-    s = (kappa_dist + one) * exp_kappa_dist * inv_dist3
+    s = exp_kappa_dist * (kappa_dist + one) * inv_dist3
 
     # A·exp(-κ·r)*([κ·r]² + 2κ·r + 2)/r³
-    d2u_dr2 = (kappa_dist*kappa_dist + two * kappa_dist + two) * exp_kappa_dist * inv_dist3
+    d2u_dr2 = exp_kappa_dist * (kappa_dist*kappa_dist + two * kappa_dist + two) * inv_dist3
 
-    return u, s, d2u_dr2  # u(r), s = -u'(r)/r, u''(r)
+    return u, s, d2u_dr2  # u(r), -u'(r)/r, u''(r)
 
 
 # Plot the Yukawa potential, and confirm the analytical derivatives
@@ -82,15 +94,17 @@ plt.figure()
 r = np.linspace(0.8, 3, 200, dtype=np.float32)
 params = [1.0, 1.0, 2.5]
 u = [yukawa(rr, params)[0] for rr in r]
+u_check = params[1] * np.exp(-params[0] * r) / r
 s = [yukawa(rr, params)[1] for rr in r]
 s_numerical = -np.gradient(u, r) / r
 umm = [yukawa(rr, params)[2] for rr in r]
 umm_numerical = np.gradient(np.gradient(u, r), r)
 plt.plot(r, u, '-', label='u(r)')
+plt.plot(r, u_check, '--', label='u(r), check')
 plt.plot(r, s, '-', label='s(r)')
-plt.plot(r, s_numerical, '--', label='s(r) numerical')
+plt.plot(r, s_numerical, '--', label='s(r), numerical')
 plt.plot(r, umm, label='u\'\'(r)')
-plt.plot(r, umm_numerical, '--', label='u\'\'(r) numerical')
+plt.plot(r, umm_numerical, '--', label='u\'\'(r), numerical')
 plt.xlabel('r')
 plt.ylabel('u, s, u\'\'')
 plt.legend()
