5776a75d804808403038eb53227817681b57259e
lorenzo-costigliola <lorenzo.costigliola@gmail.com>
2024-07-16 04:44:33 +0200

    Reduced minimal_NPT.py to be minimal. Included consistency_NPT.py with thermodynamics tests
    in examples/minimal_NPT.py removed NVT equilibration and relative output
    
    Added examples/consistency_NPT.py which runs an NVT and an NPT simulation, calculate relevant thermodynamic quantities and verifies consistency of results. The script checks that both mean values and fluctuation quantities are correct

--- examples/minimal_NPT.py
+++ examples/minimal_NPT.py
@@ -10,33 +10,7 @@ import numpy as np
 
 # Here you can decide to use "NPT_Atomic" or "NPT_Langevin"
 flag = "Atomic"
-my_T, my_rho, my_p = 2.0, 0.754289412611, 4.7
-
-# Setup configuration: FCC Lattice
-configuration = rp.Configuration()
-configuration.make_lattice(rp.unit_cells.FCC, cells=[8, 8, 8], rho=my_rho) # Pressure should be P=4.7 for T=2.0 at this density
-configuration['m'] = 1.0
-configuration.randomize_velocities(T=2.0)
-
-# Setup pair potential: Single component 12-6 Lennard-Jones
-pair_func = rp.apply_shifted_potential_cutoff(rp.LJ_12_6_sigma_epsilon)
-sig, eps, cut = 1.0, 1.0, 2.5
-pair_pot = rp.PairPotential2(pair_func, params=[sig, eps, cut], max_num_nbs=1000)
-
-# NVT equilibration
-# NOTE: steps_per_timeblock=15 generate a crash
-integrator = rp.integrators.NVT(temperature=my_T, tau=0.2, dt=0.001)
-sim = rp.Simulation(configuration, pair_pot, integrator,
-                    num_timeblocks=16, steps_per_timeblock=2048,
-                    steps_between_momentum_reset=100, scalar_output=32,
-                    storage='memory')
-sim.run()
-U, W, k = rp.extract_scalars(sim.output, ['U', 'W', 'K'], first_block=1)
-print(f"NVT at T={my_T} and \\rho={my_rho}")
-print(f"Mean values  of U, W and T_kin: {np.mean(U)/configuration.N} {np.mean(W)/configuration.N} {2*np.mean(k)/3/configuration.N}")
-print(f"Standard dev of U, W and T_kin: {np.std(U)/configuration.N} {np.std(W)/configuration.N} {2*np.std(k)/3/configuration.N}")
-print(f"Pressure: {my_rho*(2*np.mean(k)/3+np.mean(W))/configuration.N}")
-print()
+my_T, my_rho, my_p = 2.0, 0.754289412611, 4.7     # Pressure should be P=4.7 for T=2.0 at this density
 
 # Choose integrator
 match flag:
@@ -44,15 +18,35 @@ match flag:
     case "Langevin": integrator = rp.integrators.NPT_Langevin(temperature=my_T, pressure=my_p, alpha=TODO, alpha_baro=TODO, mass_baro=TODO,
                                                               volume_velocity=TODO, barostatModeISO=True, boxFlucCoord=TODO, dt=0.001, seed=TODO)
 
+print(f"\nRunning an NPT simulation using the integrator NPT_{flag} at (P, T) = ({my_p}, {my_T})\n")
+# Setup configuration: FCC Lattice
+configuration = rp.Configuration()
+configuration.make_lattice(rp.unit_cells.FCC, cells=[8, 8, 8], rho=my_rho) 
+configuration['m'] = 1.0
+configuration.randomize_velocities(T=my_T)
+
+# Setup pair potential: Single component 12-6 Lennard-Jones
+pair_func = rp.apply_shifted_potential_cutoff(rp.LJ_12_6_sigma_epsilon)
+sig, eps, cut = 1.0, 1.0, 2.5
+pair_pot = rp.PairPotential2(pair_func, params=[sig, eps, cut], max_num_nbs=1000)
+
 # NPT Simulation 
 sim = rp.Simulation(configuration, pair_pot, integrator,
                     num_timeblocks=16, steps_per_timeblock=2048,
                     steps_between_momentum_reset=100, scalar_output=32,
                     storage='memory')
+# Equilibration
+print("Equilibration run")
 sim.run()
-U, W, k = rp.extract_scalars(sim.output, ['U', 'W', 'K'], first_block=1)
-print(f"NPT at T={my_T} and p={my_p}")
-print(f"Mean values  of U, W and T_kin: {np.mean(U)/configuration.N} {np.mean(W)/configuration.N} {2*np.mean(k)/3/configuration.N}")
-print(f"Standard dev of U, W and T_kin: {np.std(U)/configuration.N} {np.std(W)/configuration.N} {2*np.std(k)/3/configuration.N}")
-print(f"Pressure: {my_rho*(2*np.mean(k)/3+np.mean(W))/configuration.N}") # This is wrong because shoudn't use my_rho but calculate average volume
+# Data
+print("Data run")
+sim.run()
+
+U, W, K, Vol = rp.extract_scalars(sim.output, ['U', 'W', 'K', 'Vol'], first_block=1)
+print(f"NPT simulation at (P, T) = ({my_p}, {my_T})")
+print(f"Mean values  of U, W and T_kin: {np.mean(U)/configuration.N} {np.mean(W)/configuration.N} {2*np.mean(K)/3/configuration.N}")
+print(f"Standard dev of U, W and T_kin: {np.std(U)/configuration.N} {np.std(W)/configuration.N} {2*np.std(K)/3/configuration.N}")
+print(f"Pressure (mean): {(2*np.mean(K)/3+np.mean(W))/np.mean(Vol)}") 
+print(f"Mean and std of Volume: {np.mean(Vol)} {np.std(Vol)}")
+print(f"Density should be {my_rho} and is {configuration.N/np.mean(Vol)}")
 
