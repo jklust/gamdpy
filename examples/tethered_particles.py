c5d8e84c8d8fc1cf2b29790b312ecddf913dffdf
urpedersen <ulf@urp.dk>
2024-07-23 09:55:42 +0200

    Made the tethered_particles.py run faster. Commented out the compute plan for old card, and reduced compute time. Made other tweeks.

--- examples/tethered_particles.py
+++ examples/tethered_particles.py
@@ -7,7 +7,6 @@
 """
 
 import numpy as np
-import random 
 import rumdpy as rp
 
 # Setup a default fcc configuration
@@ -16,7 +15,7 @@ configuration = rp.make_configuration_fcc(nxUnits, nyUnits, nzUnits, rho)
 
 # Fluid/free particles have type '0', tethered particles '1' and '2'
 for n in range(configuration.N):
-    if  configuration['r'][n][2] > -3 and configuration['r'][n][2] < -1:
+    if configuration['r'][n][2] > -3 and configuration['r'][n][2] < -1:
         configuration.ptype[n] = 1
     elif configuration['r'][n][0] > -1 and configuration['r'][n][0] < 1:
         configuration.ptype[n] = 2
@@ -27,11 +26,11 @@ rp.tools.save_configuration(configuration, "initial.xyz")
 tether = rp.Tether([1, 2], [300, 500], configuration, verbose=True)
 
 # Set the pair interactions
-pairfunc = rp.apply_shifted_potential_cutoff(rp.LJ_12_6_sigma_epsilon)
+pair_func = rp.apply_shifted_potential_cutoff(rp.LJ_12_6_sigma_epsilon)
 sig = [[1.0, 1.0, 1.0], [1.0, 1.0, 1.0], [1.0, 1.0, 1.0]] 
 eps = [[1.0, 1.0, 1.0], [1.0, 1.0, 1.0], [1.0, 1.0, 1.0]]
 cut = np.array(sig)*2.5 
-pairpot = rp.PairPotential2(pairfunc, params=[sig, eps, cut], max_num_nbs=1000)
+pair_pot = rp.PairPotential2(pair_func, params=[sig, eps, cut], max_num_nbs=1000)
 
 # Temperature
 configuration.randomize_velocities(T=2.0)
@@ -39,15 +38,18 @@ configuration.randomize_velocities(T=2.0)
 # Setup integrator: NVT
 integrator = rp.integrators.NVT(temperature=2.0, tau=0.2, dt=0.005)
 
-# Some compute plan settings for old cards 
 compute_plan = rp.get_default_compute_plan(configuration)
-compute_plan['gridsync']=False
-compute_plan['tp']=2 
+# Uncomment below for compute plan settings for old cards
+# compute_plan['gridsync']=False
+# compute_plan['tp']=2
 
-# Setup Simulation. Total number of timesteps: num_blocks * steps_per_block
-sim = rp.Simulation(configuration, [pairpot, tether], integrator,
-                    num_timeblocks=100, steps_per_timeblock=1024,
-                    steps_between_momentum_reset=0, storage='LJ_T0.70.h5', compute_plan=compute_plan)
+# Setup Simulation. Total number of time steps: num_blocks * steps_per_block
+sim = rp.Simulation(configuration, [pair_pot, tether], integrator,
+                    num_timeblocks=16,
+                    steps_per_timeblock=1024,
+                    steps_between_momentum_reset=0,  # No momentum reset needed for tethered particles
+                    storage='memory',
+                    compute_plan=compute_plan)
 
 # Run simulation one block at a time
 for block in sim.timeblocks():
