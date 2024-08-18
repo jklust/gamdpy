dad626e7feaefa1e97c70c872dc6ccda9456f2e5
Jesper Schmidt Hansen <jschmidt@ruc.dk>
2024-08-18 19:51:11 +0200

    Temporary commit

--- examples/tethered_particles.py
+++ examples/tethered_particles.py
@@ -14,14 +14,15 @@ nx, ny, nz, rho = 6, 6, 10, 1.0
 configuration = rp.Configuration(D=3)
 configuration.make_lattice(rp.unit_cells.FCC, cells=[nx, ny, nz], rho=rho)
 configuration['m'] = 1.0
+configuration.randomize_velocities(T=2.0)
+
+print(configuration['r'])
 
 # Fluid/free particles have type '0', tethered particles '1' and '2'
-hLx = configuration.simbox.lengths[0]*0.5
-hLz = configuration.simbox.lengths[2]*0.5
 for n in range(configuration.N):
-    if -3 < configuration['r'][n][2] - hLz < -1:
+    if  4 < configuration['r'][n][2] < 5:
         configuration.ptype[n] = 1
-    elif -1 < configuration['r'][n][0] - hLx < 1:
+    elif 4 < configuration['r'][n][1] < 5:
         configuration.ptype[n] = 2
 
 rp.tools.save_configuration(configuration, "initial.xyz")
@@ -38,15 +39,8 @@ eps = [[1.0, 1.0, 1.0], [1.0, 1.0, 1.0], [1.0, 1.0, 1.0]]
 cut = np.array(sig)*2.5 
 pair_pot = rp.PairPotential(pair_func, params=[sig, eps, cut], max_num_nbs=1000)
 
-# Temperature
-configuration.randomize_velocities(T=2.0)
-
 # Setup integrator: NVT
 integrator = rp.integrators.NVT(temperature=2.0, tau=0.2, dt=0.005)
-
-# Compute plan
-#compute_plan = rp.get_default_compute_plan(configuration)
-
 
 # Setup Simulation. Total number of time steps: num_blocks * steps_per_block
 sim = rp.Simulation(configuration, [pair_pot, tether], integrator,
@@ -55,9 +49,13 @@ sim = rp.Simulation(configuration, [pair_pot, tether], integrator,
                     steps_between_momentum_reset=0,  # No momentum reset needed for tethered particles
                     storage='memory')
 
+print(configuration['r'])
+
 # Run simulation one block at a time
 for block in sim.timeblocks():
     print(sim.status(per_particle=True))
+
+print(configuration['r'])
 
 print(sim.summary())
 
