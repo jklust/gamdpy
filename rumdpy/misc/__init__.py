ac4775a6cc2a1a2455f5be0918922c3b6d5dcc59
lorenzo-costigliola <lorenzo.costigliola@gmail.com>
2024-09-03 17:05:28 +0200

    restructuring part 1: misc
    split rest of misc.py

--- rumdpy/misc/__init__.py
+++ rumdpy/misc/__init__.py
@@ -1,2 +1,5 @@
 from .get_default_sim import get_default_sim
-from .misc import *
+from .get_default_compute_plan import get_default_compute_plan
+from .plot_scalars import plot_scalars
+from .make_function import make_function_constant, make_function_ramp, make_function_sin
+#from .misc import *
