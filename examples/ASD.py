6067324acfecd2e916d695029be95b29fb8d8c4e
tbs.cph <tbs@ruc.dk>
2025-01-19 12:41:56 +0100

    First stab at runtime_actions_interface, i.e. Simulation gets a list of runtime_actions when initilised. Only implemented in examples minimal.py and ASD.py so far.

--- examples/ASD.py
+++ examples/ASD.py
@@ -62,6 +62,13 @@ for block in sim.run_timeblocks():
         print(sim.status(per_particle=True))
 print(sim.summary())
 
+#integrator = rp.integrators.NVT(temperature=temperature, tau=0.2, dt=dt)
+integrator = rp.integrators.NVE(dt=dt)
+sim = rp.Simulation(configuration, [pair_pot, bonds], integrator,
+                    runtime_actions=[rp.MomentumReset(100), rp.ConfSaver(), rp.ScalarSaver()],
+                    num_timeblocks=num_blocks, steps_per_timeblock=steps_per_block,
+                    compute_flags={'Fsq':True, 'lapU':True, 'Ptot':True},
+                    storage=filename)
 print('Production:')
 integrator = rp.integrators.NVT(temperature=temperature, tau=0.2, dt=dt)
 
