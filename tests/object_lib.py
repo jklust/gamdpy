a23a249633d06d0b2e110841eb03646ba09a8b36
lorenzo-costigliola <lorenzo.costigliola@gmail.com>
2025-01-29 16:28:48 +0100

    modified tests/object_lib.py to make it behave as a smaller module. Only configuration_SC and pairpot_LJ are visible in userspace

--- tests/object_lib.py
+++ tests/object_lib.py
@@ -1,13 +1,19 @@
-import rumdpy as rp
+# This is a library of predefined rumdpy classes which can be used in tests
+__all__ = ["configuration_SC",
+           "pairpot_LJ"]
 
+from rumdpy import Configuration as _Configuration
 # Set up a 3d configuration for a single component system
-configuration_SC = rp.Configuration(D=3, compute_flags={'W':True})
+configuration_SC = _Configuration(D=3, compute_flags={'W':True})
 configuration_SC.make_positions(N=1000, rho=0.754)
 configuration_SC['m'] = 1.0  # Set all masses to 1.0
 configuration_SC.randomize_velocities(temperature=2.0)
 
+from rumdpy import apply_shifted_potential_cutoff as _apply_shifted_potential_cutoff
+from rumdpy import LJ_12_6_sigma_epsilon as _LJ_12_6_sigma_epsilon
+from rumdpy import PairPotential as _PairPotential
 # Set up a LJ pair potential
-pairfunc = rp.apply_shifted_potential_cutoff(rp.LJ_12_6_sigma_epsilon)
-sig, eps, cut = 1.0, 1.0, 2.5
-pairpot_LJ = rp.PairPotential(pairfunc, params=[sig, eps, cut], max_num_nbs=1000)
+_pairfunc = _apply_shifted_potential_cutoff(_LJ_12_6_sigma_epsilon)
+_sig, _eps, _cut = 1.0, 1.0, 2.5
+pairpot_LJ = _PairPotential(_pairfunc, params=[_sig, _eps, _cut], max_num_nbs=1000)
 
