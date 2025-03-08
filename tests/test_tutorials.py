113eedecf30640ec7f3abb03714cab48b3073b51
urpedersen <urp@ruc.dk>
2025-03-08 09:22:54 +0100

    Bugfix. Removed kernel_name input in NotebookClient for CI to work (not needed according to documentation).

--- tests/test_tutorials.py
+++ tests/test_tutorials.py
@@ -6,7 +6,7 @@ def test_tutorials():
     filename = "./tutorials/my_first_simulation.ipynb"
     as_version = 4
     nb = nbformat.read(filename, as_version=as_version)
-    client = NotebookClient(nb, timeout=600, kernel_name='python3', resources={'metadata': {'path': './tutorials/'}})
+    client = NotebookClient(nb, timeout=600, resources={'metadata': {'path': './tutorials/'}})
     client.execute()
 
 if __name__ == "__main__":
