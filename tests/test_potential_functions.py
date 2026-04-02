b510d1fd7b5c8fd35533792c4873e1dd5c44d48c
urpedersen <urp@ruc.dk>
2026-04-02 09:59:05 +0200

    Made pair_potential test individual (from one long test). Add test for SAAP parameters.

--- tests/test_potential_functions.py
+++ tests/test_potential_functions.py
@@ -1,37 +1,38 @@
 import numpy as np
+import gamdpy as gp
 
-def test_potential_functions() -> None:
-    import gamdpy as gp
-    import numba
-
+def test_potential_functions_lennard_jones() -> None:
     # note: this example assumes these functions were implemented correctly in version bfa77f6e
     assert gp.LJ_12_6(1, [2, 3]) == (5.0, 42.0, 438.0), "Problem with gp.LJ_12_6"
     assert gp.LJ_12_6_sigma_epsilon(1, [2, 3]) == (48384.0, 585216.0, 7635456.0), "Problem with gp.LJ_12_6_sigma_epsilon"
     # gp.LJ_12_6_params_from_sigma_epsilon_cutoff seems not to be used
     #assert gp.LJ_12_6_params_from_sigma_epsilon_cutoff(1, [2, 3, 4]) == (5.0, 42.0, 438.0), "Problem with gp.LJ_12_6_params_from_sigma_epsilon_cutoff"
     # consider moving inner functions out for better testing
-    
-    # Test make_IPL_n
+
+def test_make_potential_function_ipl_n() -> None:
     for n, r, a in [(12,1,1), (12,2**(1/6),3), (6,2**(1/6),1), (6,2,4), (1,2,3)]:
         ipl_n = gp.make_IPL_n(n)
         expected = (a*r**(-n), n*a*r**(-n-2), a*n*(n+1)*r**(-n-2))
         assert np.all(np.isclose(ipl_n(r, (a,)), expected)), f'Problem with make_IPL_n, {(n,r,a)=}'
+    assert callable(gp.make_IPL_n(12)), "Problem with gp.make_IPL_n"
 
-    # Test add_potential_functions
+def test_add_potential_functions() -> None:
     LJ = gp.add_potential_functions(gp.make_IPL_n(12), gp.make_IPL_n(6, first_parameter=1))
     for r, a12, a6 in [(1,1,-1), (2**(1/6),3,-3), (2**(1/6),4,-4), (2,4,4), (2,4,-4)]:
         expected = gp.LJ_12_6(r, (a12, a6))
         assert np.all(np.isclose(LJ(r, (a12,a6)), expected, atol=1e-5)), f'Problem with  add_potential_functions, {(n,a12,a6)=}'
 
+def test_bond_function_harmonic() -> None:
     assert gp.harmonic_bond_function(2.5, [2, 100]) == (12.5, -20.0, 100.0), "Problem with gp.harmonic_bond_function"
     # seems correct way: https://stackoverflow.com/questions/624926/how-do-i-detect-whether-a-variable-is-a-function
-    assert callable(gp.make_IPL_n(12)), "Problem with gp.make_IPL_n"
+
+def test_make_potential_function_from_sympy() -> None:
     from sympy.abc import r,s,e
     potLJ = 4*e*((s/r)**(12)-(s/r)**6)
     potLJ_gp = gp.make_potential_function_from_sympy(potLJ, (s, e))
     assert potLJ_gp(1, (2,3)) == gp.LJ_12_6_sigma_epsilon(1, [2, 3]), "Problem with gp.make_potential_function_from_sympy"
 
-    # Test Yukawa potential
+def test_potential_function_yukawa() -> None:
     params = [1.23, 0.891]
     dist = 3.45
     pot_yukawa = gp.yukawa(dist, params)
@@ -43,14 +44,7 @@ def test_potential_functions() -> None:
     assert np.isclose(pot_yukawa[1], 0.0061450177), "Problem with gp.yukawa"
     assert np.isclose(pot_yukawa[2], 0.02499608), "Problem with gp.yukawa"
 
-    # Test SAAP potential
-    number_of_params = 8
-    params = [1.0]*number_of_params
-    dist = 1.0
-    pot_SAAP = gp.SAAP(dist, params)
-    assert len(pot_SAAP) == 3, "Problem with gp.SAAP"
-
-    # Test the universal ZBL potential
+def test_potential_function_zbl() -> None:
     sigma, epsilon = params = 1.23, 0.92
     r = 1.364
     pot_zbl = gp.universal_zbl_potential(r, params)
@@ -64,9 +58,10 @@ def test_potential_functions() -> None:
     assert np.isclose(pot_zbl[1], 0.3020571)
     assert np.isclose(pot_zbl[2], 0.73724324)
 
-    # Test harmonic repulsion, here u=(1-r)²
-    pair_pot = gp.PairPotential(gp.harmonic_repulsion, params=params, max_num_nbs=128)
+def test_potential_function_harmonic_repulsion() -> None:
+    # u=(1-r)²
     params = 2.0, 1.0
+    pair_pot = gp.PairPotential(gp.harmonic_repulsion, params=params, max_num_nbs=128)
     dist = 0.5
     pot_harm_rep = gp.harmonic_repulsion(dist, params)
     assert np.isclose(pot_harm_rep[0],0.25), "Problem with gp.harmonic_repulsion"
@@ -80,7 +75,8 @@ def test_potential_functions() -> None:
     assert np.isclose(pot_harm_rep_2[1], -du_dr/r), "Problem with gp.harmonic_repulsion"
     assert np.isclose(pot_harm_rep_2[2], eps/sig**2), "Problem with gp.harmonic_repulsion"
 
-    # Test Hertzian pair potential, u=eps*(1-r/sig)**alpha
+def test_potential_function_hertzian() -> None:
+    # u=eps*(1-r/sig)**alpha
     params = 1.0, 2.0, 1.0  # Same as "harmonic repulsion" above
     dist = 0.5
     pot_hertzian = gp.hertzian(dist, params)
@@ -96,5 +92,33 @@ def test_potential_functions() -> None:
 
     # needs to add test for apply_shifted_force_cutoff, apply_shifted_potential_cutoff
 
+def test_potential_function_saap() -> None:
+    number_of_params = 8
+    params = [1.0]*number_of_params
+    dist = 1.0
+    pot_SAAP = gp.SAAP(dist, params)
+    assert len(pot_SAAP) == 3, "Problem with gp.SAAP"
+
+def test_potential_function_saap_params() -> None:
+    prm = gp.interactions.potential_parameters.SAAP_Deiters2019_params
+    expected_keys={'comment', 'reference', 'units', 'Ne', 'Ar', 'Kr', 'Xe'}
+    for key in expected_keys:
+        assert key in prm.keys(), f"Problem with SAAP_Deiters2019_params, missing key: {key}"
+    for element in 'Ne', 'Ar', 'Kr', 'Xe':
+        this = prm[element]
+        expected_keys = {'name', 'Z', 'M', 'eps', 'sig', 'a0', 'a1', 'a2', 'a3', 'a4', 'a5'}
+        for key in expected_keys:
+            assert key in this.keys(), f"Problem with SAAP_Deiters2019_params['{element}'] missing key: {key}"
+
 if __name__ == '__main__':  # pragma: no cover
-    test_potential_functions()
+    test_potential_functions_lennard_jones()
+    test_add_potential_functions()
+    test_bond_function_harmonic()
+    test_make_potential_function_ipl_n()
+    test_make_potential_function_from_sympy()
+    test_potential_function_yukawa()
+    test_potential_function_zbl()
+    test_potential_function_harmonic_repulsion()
+    test_potential_function_hertzian()
+    test_potential_function_saap()
+    test_potential_function_saap_params()
